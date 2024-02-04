5df9adc3fcd15d08d5c40ce0bf5bbd5320d42b1a
Gyeongseo Bak <rudtj2966@gmail.com>
2024-02-04 17:31:55 +0900

    [Feat] : Util 추가
    - 상수 토큰 헤더 값 추가
    - 파일 랜덤 이름 생성 Util

---
+++ src/main/java/kr/aling/file/common/util/FileInfoUtil.java
@@ -0,0 +1,35 @@
+package kr.aling.file.common.util;
+
+import javax.servlet.http.Part;
+import kr.aling.file.common.dto.FileInfoDto;
+import org.apache.commons.lang3.RandomStringUtils;
+
+/**
+ * 파일 정보를 생성 하는 Util Class.
+ *
+ * @author : 박경서
+ * @since : 1.0
+ **/
+public class FileInfoUtil {
+
+    /**
+     * Util Class 기본 생성자.
+     */
+    private FileInfoUtil() {
+    }
+
+    /**
+     * 파일의 이름, 무작위 파일 이름 생성하는 메서드.
+     *
+     * @param part 파일
+     * @return 원본 이름, 저장 이름 Dto
+     */
+    public static FileInfoDto generateFileInfo(Part part) {
+        String originFileName = part.getSubmittedFileName();
+        String extension = originFileName.substring(originFileName.lastIndexOf("."));
+        String saveFileName = RandomStringUtils.randomAlphanumeric(20) + "-"
+                + RandomStringUtils.randomAlphabetic(20) + extension;
+
+        return new FileInfoDto(originFileName, saveFileName);
+    }
+}
