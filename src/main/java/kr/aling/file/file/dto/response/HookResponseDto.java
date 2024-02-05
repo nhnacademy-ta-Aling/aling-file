a9947ba934a653244d25e797bfb5557673a751d4
Gyeongseo Bak <rudtj2966@gmail.com>
2024-02-05 21:47:39 +0900

    [Feat] : Dto 작성
    - Hook 이미지 응답

---
+++ src/main/java/kr/aling/file/file/dto/response/HookResponseDto.java
@@ -0,0 +1,17 @@
+package kr.aling.file.file.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * Toast Ui Hook 이미지 응답 Dto.
+ *
+ * @author : 박경서
+ * @since : 1.0
+ **/
+@Getter
+@AllArgsConstructor
+public class HookResponseDto {
+
+    private String path;
+}
