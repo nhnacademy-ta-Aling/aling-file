53e57ab1a49b8781e49313a0fdb6af41ab9d636a
Gyeongseo Bak <rudtj2966@gmail.com>
2024-02-05 21:47:25 +0900

    [Feat] : Hook 이미지 저장

--- src/main/java/kr/aling/file/file/service/FileFacadeService.java
+++ src/main/java/kr/aling/file/file/service/FileFacadeService.java
@@ -1,8 +1,10 @@
 package kr.aling.file.file.service;
 
 import javax.servlet.http.HttpServletRequest;
+import kr.aling.file.file.dto.response.HookResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * File 파사드 서비스.
@@ -25,5 +27,17 @@ public class FileFacadeService {
      */
     public void uploadFiles(HttpServletRequest request, Integer fileCategoryNo, String fileSaveLocation) {
         fileServiceResolver.chooseFileService(fileSaveLocation).saveFile(request, fileCategoryNo);
+    }
+
+    /**
+     * Hook Image 업로드.
+     *
+     * @param multipartFile      MultipartFile
+     * @param fileCategoryNo     파일 Category 번호
+     * @param fileSaveLocation   파일 저장 위치
+     * @return Hook 응답 Dto
+     */
+    public HookResponseDto uploadHookImage(MultipartFile multipartFile, Integer fileCategoryNo, String fileSaveLocation) {
+        return fileServiceResolver.chooseFileService(fileSaveLocation).saveOnlyHookImageFile(multipartFile, fileCategoryNo);
     }
 }
