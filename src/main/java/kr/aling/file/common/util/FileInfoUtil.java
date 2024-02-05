83cdded2c40c83265b0caffc3abe6b4c267c10b2
Gyeongseo Bak <rudtj2966@gmail.com>
2024-02-05 21:47:57 +0900

    [Feat] : Util 추가
    - MultipartFile용 메서드 추가

--- src/main/java/kr/aling/file/common/util/FileInfoUtil.java
+++ src/main/java/kr/aling/file/common/util/FileInfoUtil.java
@@ -1,8 +1,10 @@
 package kr.aling.file.common.util;
 
+import java.util.Objects;
 import javax.servlet.http.Part;
 import kr.aling.file.common.dto.FileInfoDto;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * 파일 정보를 생성 하는 Util Class.
@@ -27,6 +29,21 @@ public class FileInfoUtil {
     public static FileInfoDto generateFileInfo(Part part) {
         String originFileName = part.getSubmittedFileName();
         String extension = originFileName.substring(originFileName.lastIndexOf("."));
+        String saveFileName = RandomStringUtils.randomAlphanumeric(20) + "-"
+                + RandomStringUtils.randomAlphabetic(20) + extension;
+
+        return new FileInfoDto(originFileName, saveFileName);
+    }
+
+    /**
+     * MultipartFile 가지고 파일의 이름, 무작위 파일 이름 생성 하는 메서드.
+     *
+     * @param multipartFile MultipartFile
+     * @return 원본 이름, 저장 이름 Dto
+     */
+    public static FileInfoDto generateFileInfo(MultipartFile multipartFile) {
+        String originFileName = multipartFile.getOriginalFilename();
+        String extension = Objects.requireNonNull(originFileName).substring(originFileName.lastIndexOf("."));
         String saveFileName = RandomStringUtils.randomAlphanumeric(20) + "-"
                 + RandomStringUtils.randomAlphabetic(20) + extension;
 
