19ee15fa1329fb32cf78751e30eff4a7a843e8ee
Gyeongseo Bak <rudtj2966@gmail.com>
2024-02-11 21:13:20 +0900

    [Feat] : Dto 추가
    - 파일 업로드 응답 Dto

---
+++ src/main/java/kr/aling/file/file/dto/response/FileUploadResponseDto.java
@@ -0,0 +1,16 @@
+package kr.aling.file.file.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 파일 업로드 응답 Dto.
+ *
+ * @author : 박경서
+ * @since : 1.0
+ **/
+@Getter
+@AllArgsConstructor
+public class FileUploadResponseDto {
+    private Long fileNo;
+}
